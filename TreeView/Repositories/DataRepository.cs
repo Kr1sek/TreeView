a595d3ba3f0be24f8d284f2fb49761ca7646caa0
Krystian Moskal <w64160@student.wsiz.edu.pl>
2022-06-16 22:25:40 +0200

    add comments

--- TreeView/Repositories/DataRepository.cs
+++ TreeView/Repositories/DataRepository.cs
@@ -14,18 +14,22 @@ namespace TreeView.Repositories
         {
             _context = context;
         }
+        //Metoda do pobrania elementu o konkretnym Id
         public DataModel Get(int dataId)
             => _context.Datas.SingleOrDefault(x => x.DataId.Equals(dataId));
 
+        //Metoda do pobrania wszystkich elementów
         public IQueryable<DataModel> GetAll()
             => _context.Datas;
 
+        //Metoda do dodania elementu
         public void Add(DataModel data)
         {
             _context.Datas.Add(data);
             _context.SaveChanges();
         }
 
+        //Metoda do usunięcia elementu
         public void Delete(int dataId)
         {
             var result = _context.Datas.SingleOrDefault(x => x.DataId.Equals(dataId));
@@ -36,6 +40,7 @@ namespace TreeView.Repositories
             }
         }
 
+        //Metoda do edycji elementu
         public void Update(int dataId, DataModel data)
         {
             var result = _context.Datas.SingleOrDefault(x => x.DataId.Equals(dataId));
@@ -46,7 +51,9 @@ namespace TreeView.Repositories
                 _context.SaveChanges();
             }
         }
-        public void FakeData()
+
+        // Wypełnij danymi testowymi
+        public void FakeData() 
         {
             //Random rnd = new Random();
             //for (int i = 0; i < 10; i++)
--- TreeView/Startup.cs
+++ TreeView/Startup.cs
@@ -51,7 +51,7 @@ namespace TreeView
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Data}/{action=Index}/{id?}");
+                    pattern: "{controller=Data}/{action=Index}/{id?}"); //Wyœwietlenie strony startowej
             });
         }
     }
